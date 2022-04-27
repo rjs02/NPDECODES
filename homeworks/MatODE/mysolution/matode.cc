e15e492f7fd5a0a02a6c27c31673d2afc925b7d5
Robin <rjs02@hotmail.com>
2022-04-27 18:38:10 +0200

    add 6-1 solutions

--- homeworks/MatODE/mysolution/matode.cc
+++ homeworks/MatODE/mysolution/matode.cc
@@ -13,8 +13,12 @@ Eigen::MatrixXd eeulstep(const Eigen::MatrixXd& A, const Eigen::MatrixXd& Y0,
                          double h) {
   //====================
   // Your code goes here
+
+  // explicit Euler step
+  return Y0 + h * A * Y0;
+
   //====================
-  return Y0;
+  //return Y0;
 }
 /* SAM_LISTING_END_3 */
 
@@ -23,8 +27,13 @@ Eigen::MatrixXd ieulstep(const Eigen::MatrixXd& A, const Eigen::MatrixXd& Y0,
                          double h) {
   //====================
   // Your code goes here
+
+  // implicit Euler step
+  
+  return (Eigen::MatrixXd::Identity(Y0.rows(), Y0.cols()) - h*A).lu().solve(Y0);
+
   //====================
-  return Y0;
+  //return Y0;
 }
 /* SAM_LISTING_END_4 */
 
@@ -33,8 +42,11 @@ Eigen::MatrixXd impstep(const Eigen::MatrixXd& A, const Eigen::MatrixXd& Y0,
                         double h) {
   //====================
   // Your code goes here
+
+  return (Eigen::MatrixXd::Identity(Y0.rows(), Y0.cols()) - h/2. * A).lu().solve(Y0 + h/2. * A * Y0);
+
   //====================
-  return Y0;
+  //return Y0;
 }
 /* SAM_LISTING_END_5 */
 
--- homeworks/MatODE/mysolution/matode_main.cc
+++ homeworks/MatODE/mysolution/matode_main.cc
@@ -14,7 +14,7 @@
 int main(int /*argc*/, char** /*argv*/) {
   /* SAM_LISTING_BEGIN_6 */
   double h = 0.01;  // stepsize
-  Eigen::Vector3d norms;
+  //Eigen::Vector3d norms;
   // Build M
   Eigen::Matrix3d M;
   M << 8, 1, 6, 3, 5, 7, 9, 9, 2;
@@ -25,6 +25,33 @@ int main(int /*argc*/, char** /*argv*/) {
   //====================
   // Your code goes here
   //====================
+  
+  using namespace MatODE;
+
+  // y0 is Q in M = QR
+  Eigen::MatrixXd Y01, Y02, Y03;
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(M);
+  Y01 = Y02 = Y03 = qr.householderQ();
+
+  Eigen::MatrixXd norms = Eigen::MatrixXd::Zero(20, 3); // stores frobenius norms using 3 different methods
+  // perform 20 steps using the implemented methods
+  for(int i = 0; i < 20; ++i) {
+    // explicit Euler step
+    Eigen::MatrixXd Y1 = eeulstep(A, Y01, h);
+    norms(i, 0) = (Y1.transpose()*Y1 - I).norm();
+    Y01 = Y1;
+    // implicit Euler step
+    Y1 = ieulstep(A, Y02, h);
+    norms(i, 1) = (Y1.transpose()*Y1 - I).norm();
+    Y02 = Y1;
+    // implicit Euler step
+    Y1 = impstep(A, Y03, h);
+    norms(i, 2) = (Y1.transpose()*Y1 - I).norm();
+    Y03 = Y1;
+  }
+
+  std::cout << "Norms:" << std::endl;
+  std::cout << norms << std::endl;
   /* SAM_LISTING_END_6 */
   return 0;
 }
